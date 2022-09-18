edcfd7de56df1f4b9f3fb58dd6d8453a8af08f74
oichan <makoto.uwaya@gmail.com>
2022-09-18 19:06:42 +0900

    ✨ Add Bottom Navigation Bar
    - Add mulutiple screens

---
+++ lib/src/screens/information.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class InformationScreen extends StatefulWidget {
+  const InformationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InformationScreen> createState() => _InformationScreenState();
+}
+
+class _InformationScreenState extends State<InformationScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
