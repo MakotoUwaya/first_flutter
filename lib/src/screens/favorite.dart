eba0467e0feea8c10dd7d5c2c504d73204ea0335
oichan <makoto.uwaya@gmail.com>
2022-09-19 11:49:18 +0900

    🔥 Remove WebScrollbar

--- lib/src/screens/favorite.dart
+++ lib/src/screens/favorite.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_application_1/src/widgets/web_scrollbar.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteScreen extends StatefulWidget {
@@ -9,44 +8,20 @@ class FavoriteScreen extends StatefulWidget {
 }
 
 class _FavoriteScreenState extends State<FavoriteScreen> {
-  late ScrollController _scrollController;
-  double _scrollPosition = 0;
-  double _opacity = 0;
-
-  _scrollListener() {
-    setState(() {
-      _scrollPosition = _scrollController.position.pixels;
-    });
-  }
-
-  @override
-  void initState() {
-    _scrollController = ScrollController();
-    _scrollController.addListener(_scrollListener);
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    var screenSize = MediaQuery.of(context).size;
-    _opacity = _scrollPosition < screenSize.height * 0.40
-        ? _scrollPosition / (screenSize.height * 0.40)
-        : 1;
-
     return Scaffold(
-      body: WebScrollbar(
-          controller: _scrollController,
-          child: GridView.count(
-            crossAxisCount: 3,
-            children: List.generate(100, (index) {
-              return Center(
-                child: Text(
-                  'Item $index',
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-              );
-            }),
-          )),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(100, (index) {
+          return Center(
+            child: Text(
+              'Item $index',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          );
+        }),
+      ),
     );
   }
 }
